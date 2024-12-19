f2ed94a0ed01382f6a1b4914d274b2822f952cbf
dhakalasace <dhakalasace777@gmail.com>
2024-12-19 11:49:46 -0600

    Updated SQL file with comments and added connection instructions

--- sql/deliverable3_db.sql
+++ sql/deliverable3_db.sql
@@ -1,11 +1,17 @@
-﻿DROP TABLE IF EXISTS customers;
+﻿-- Drop the customers table if it exists
+DROP TABLE IF EXISTS customers;
 
+-- Create the customers table
 CREATE TABLE customers(
 	phoneNumber VARCHAR(15) PRIMARY KEY,
 	address VARCHAR(30) NOT  NULL,
 	name VARCHAR(30) NOT  NULL
 );
+
+-- Set the owner of the customers table
 ALTER TABLE customers OWNER TO db_user;
+
+-- Insert sample data into the customers table
 INSERT INTO customers(phoneNumber, address, name) VALUES ('123-4567','Toronto','Eleanor');
 INSERT INTO customers(phoneNumber, address, name) VALUES ('765-4321','Montreal','Dave');
 INSERT INTO customers(phoneNumber, address, name) VALUES ('222-2222','Halifax','Mike');
@@ -13,4 +19,5 @@ INSERT INTO customers(phoneNumber, address, name) VALUES ('333-3333','Winnipeg',
 INSERT INTO customers(phoneNumber, address, name) VALUES ('444-4444','Vancouver','Dan');
 INSERT INTO customers(phoneNumber, address, name) VALUES ('555-5555','Calgary','JoAnn');
 
+-- Select all records from the customers table
 SELECT * FROM customers;
